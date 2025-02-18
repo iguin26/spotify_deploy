1e430bcebb47deff0ef5d408e7112f55ca8009b5
iguin26 <igordaniel.lopes@gmail.com>
2025-02-18 02:13:53 -0300

    ajustes

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import axios from "axios";
 
-const URL = "http://localhost:3000/api";
+// const URL = "http://localhost:3000/api";
+const URL = "https://spotify-deploy-q2u1.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);

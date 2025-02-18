3763124a7d55756041c08a2b33a73f2a0a7803fe
iguin26 <igordaniel.lopes@gmail.com>
2025-02-18 02:05:38 -0300

    deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,6 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-
 const URL = "http://localhost:3000/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
